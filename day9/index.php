d857fbc0e5c3805e2e75a64ef60e53ae2a6fa732
Will Hawkins <wh16862@my.bristol.ac.uk>
2021-12-08 21:58:28 +0000

    adding a skeleton for day 9 in preparation

---
+++ day9/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    $file = file_get_contents("sampleInput.txt", true);
+    $file = file_get_contents("input.txt", true);
+    $allInputs = explode("\n", $file);
+
+
+    //PART ONE
+
+
+    echo "<br>";
+
+    //PART TWO
+
+
+    //END
+
+    //FUNCTIONS
+
+?>
